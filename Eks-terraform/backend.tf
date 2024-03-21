ff6a45155573217b7eaec6a1286a337033abf0d0
Gatabaki <120761479+Gatabaki@users.noreply.github.com>
2024-03-21 11:48:59 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "gatabaki_cloud2024" # Replace with your actual S3 bucket name
+    bucket = "gatabakicloud2024" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
